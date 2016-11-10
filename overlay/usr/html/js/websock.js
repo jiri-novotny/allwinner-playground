526503435d1733c7903762d45bbb9577b284d5aa
Jiri Novotny <jiri.novotny@logicelements.cz>
2016-11-10 21:56:58 +0100

    Initial commit
    Signed-off-by: Jiri Novotny <jiri.novotny@logicelements.cz>

---
+++ overlay/usr/html/js/websock.js
@@ -0,0 +1,41 @@
+if ("WebSocket" in window)
+{
+  //alert("WebSocket is supported by your Browser!");
+
+  // Let us open a web socket
+  var ws = new WebSocket("ws://192.168.0.10:3000");
+
+  ws.onopen = function()
+  {
+    // Web Socket is connected, send data using send()
+    ws.send("init");
+  };
+
+  ws.onmessage = function (evt) 
+  { 
+    var resp = evt.data.split(";");
+    
+    for (i = 0; i < resp.length; i++)
+    { 
+      var node = resp[i].split("=");
+      document.getElementById(node[0]).innerHTML = node[1];
+    }
+  };
+
+  ws.onclose = function()
+  { 
+    // websocket is closed.
+    alert("WS connection closed!"); 
+  };
+  
+  window.onbeforeunload = function() {
+    websocket.onclose = function () {}; // disable onclose handler first
+    websocket.close()
+  };
+}
+   
+else
+{
+  // The browser doesn't support WebSocket
+  alert("WebSocket NOT supported by your Browser!");
+}
