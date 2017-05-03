d7e611001104d802d159f93fc20d80dc609c5677
Jiri Novotny <jiri.novotny@logicelements.cz>
2017-05-03 21:55:15 +0200

    add h2 rootfs and kernel config
    Signed-off-by: Jiri Novotny <jiri.novotny@logicelements.cz>

--- src/allwinner/board/h2zero/post-build.sh
+++ src/allwinner/board/h2zero/post-build.sh
@@ -6,6 +6,10 @@ echo HOST_KERNEL_RELEASE=`uname -r` >> ${TARGET_DIR}/etc/build
 echo HOST_KERNEL_VERSION=`uname -v` >> ${TARGET_DIR}/etc/build
 echo BUILD_DATE=`date -R` >> ${TARGET_DIR}/etc/build
 
+# buildroot extra tools
+cp ${BUILD_DIR}/binutils*/binutils/objdump ${TARGET_DIR}/usr/bin
+cp ${STAGING_DIR}/usr/lib/libopcodes-*.so ${TARGET_DIR}/usr/lib
+
 # remove links from target fs
 if [ -f ${TARGET_DIR}/linuxrc ]; then
   rm -rf ${TARGET_DIR}/linuxrc
