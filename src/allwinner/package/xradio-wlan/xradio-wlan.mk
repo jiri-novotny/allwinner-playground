96bcf3103d054d31506a585fd56d6560740149e2
Jiri Novotny <jiri.novotny@logicelements.cz>
2017-07-05 21:46:46 +0200

    update xr819 package and add fw
    Signed-off-by: Jiri Novotny <jiri.novotny@logicelements.cz>

--- src/allwinner/package/xradio-wlan/xradio-wlan.mk
+++ src/allwinner/package/xradio-wlan/xradio-wlan.mk
@@ -14,6 +14,26 @@ define XRADIO_WLAN_BUILD_CMDS
 	-C $(LINUX_DIR) M=$(@D)
 endef
 
+ifneq ($(BR2_XRADIO_WLAN_COPY_FW),y)
+define XRADIO_WLAN_COPY_FW
+	if [ ! -d $(TARGET_DIR)/lib/firmware ]; then \
+		mkdir $(TARGET_DIR)/lib/firmware; \
+	fi
+	if [ ! -d $(TARGET_DIR)/lib/firmware/xr819 ]; then \
+		mkdir $(TARGET_DIR)/lib/firmware/xr819; \
+	fi
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_ALLWINNER_PATH)/package/xradio-wlan/firmware/boot_B100.bin \
+		$(TARGET_DIR)/lib/firmware/xr819/
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_ALLWINNER_PATH)/package/xradio-wlan/firmware/etf_B100.bin \
+		$(TARGET_DIR)/lib/firmware/xr819/etf_B100.bin
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_ALLWINNER_PATH)/package/xradio-wlan/firmware/fw_B100.bin \
+		$(TARGET_DIR)/lib/firmware/xr819/fw_B100.bin
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_ALLWINNER_PATH)/package/xradio-wlan/firmware/sdd_B100.bin \
+		$(TARGET_DIR)/lib/firmware/xr819/sdd_B100.bin
+endef
+XRADIO_WLAN_POST_INSTALL_TARGET_HOOKS += XRADIO_WLAN_COPY_FW
+endif
+
 define XRADIO_WLAN_INSTALL_TARGET_CMDS
 	if [ ! -d $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra ]; then \
 		mkdir $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/extra; \
