de93acc1bf621ab2ae72352c4dd852633d1f73bb
LE/Jiri Novotny <jiri.novotny@logicelements.cz>
2020-10-06 20:55:15 +0200

    licheenano: add i2c, improve usb, use usb net
    Signed-off-by: LE/Jiri Novotny <jiri.novotny@logicelements.cz>

--- src/extra/board/licheenano/post-build.sh
+++ src/extra/board/licheenano/post-build.sh
@@ -3,7 +3,7 @@
 if [ -f ${LINUX_DIR}/arch/arm/boot/dts/suniv-f1c100s-licheepi-nano.dtb ]; then
   cp ${LINUX_DIR}/arch/arm/boot/dts/suniv-f1c100s-licheepi-nano.dtb ${BINARIES_DIR}
 else
-  cp ${BUILD_DIR}/linux-5.9-rc4/arch/arm/boot/dts/suniv-f1c100s-licheepi-nano.dtb ${BINARIES_DIR}  
+  cp ${BUILD_DIR}/linux-5.9-rc6/arch/arm/boot/dts/suniv-f1c100s-licheepi-nano.dtb ${BINARIES_DIR}  
 fi
 
 cp ${BINARIES_DIR}/suniv-f1c100s-licheepi-nano.dtb ${TARGET_DIR}/boot
