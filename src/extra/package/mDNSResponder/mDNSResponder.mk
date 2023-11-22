f0a62ac24ab7923fbf6fa77831c8091c38e7f990
Jiri Novotny <jiri.novotny@logicelements.cz>
2023-11-22 20:21:28 +0100

    mdnsresponder: package fixups
    Signed-off-by: Jiri Novotny <jiri.novotny@logicelements.cz>

--- src/extra/package/mDNSResponder/mDNSResponder.mk
+++ src/extra/package/mDNSResponder/mDNSResponder.mk
@@ -1,16 +1,16 @@
 ################################################################################
 #
-# mdnsd
+# mDNSResponder
 #
 ################################################################################
 
-MDNSD_VERSION = 1310.40.42
-MDNSD_SOURCE = mDNSResponder-$(MDNSD_VERSION).tar.gz
-MDNSD_SITE = https://opensource.apple.com/tarballs/mDNSResponder
-MDNSD_LICENSE = Apache 2.0
-MDNSD_INSTALL_STAGING = YES
+MDNSRESPONDER_VERSION = 1310.40.42
+MDNSRESPONDER_SOURCE = mDNSResponder-$(MDNSRESPONDER_VERSION).tar.gz
+MDNSRESPONDER_SITE = https://opensource.apple.com/tarballs/mDNSResponder
+MDNSRESPONDER_LICENSE = Apache 2.0
+MDNSRESPONDER_INSTALL_STAGING = YES
 
-define MDNSD_BUILD_CMDS
+define MDNSRESPONDER_BUILD_CMDS
 	$(TARGET_MAKE_ENV) CFLAGS="$(TARGET_CFLAGS)" \
 	LDFLAGS="$(TARGET_LDFLAGS)" \
 	CC="$(TARGET_CC)" \
@@ -21,16 +21,16 @@ define MDNSD_BUILD_CMDS
 	$(MAKE1) os=buildroot -C $(@D)/mDNSPosix
 endef
 
-define MDNSD_INSTALL_STAGING_CMDS
+define MDNSRESPONDER_INSTALL_STAGING_CMDS
 	cp -dpfr $(@D)/mDNSShared/dns_sd.h $(STAGING_DIR)/usr/include
 	cp -dpfr $(@D)/mDNSPosix/build/prod/libdns_sd.so $(STAGING_DIR)/usr/lib
 endef
 
-define MDNSD_INSTALL_TARGET_CMDS
+define MDNSRESPONDER_INSTALL_TARGET_CMDS
 	cp -dpfr $(@D)/mDNSPosix/build/prod/libdns_sd.so $(TARGET_DIR)/usr/lib
 	cp -dpfr $(@D)/mDNSPosix/build/prod/mdnsd $(TARGET_DIR)/usr/sbin
 	cp -dpfr $(@D)/Clients/build/dns-sd $(TARGET_DIR)/usr/sbin
-	cp -dpfr $(BR2_EXTERNAL_EXTRAS_PATH)/package/mdnsd/S42mdnsd $(TARGET_DIR)/etc/init.d
+	cp -dpfr $(BR2_EXTERNAL_EXTRAS_PATH)/package/mDNSResponder/S42mDNSResponder $(TARGET_DIR)/etc/init.d
 endef
 
 $(eval $(generic-package))
